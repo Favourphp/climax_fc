8eda4d40d9ec6cdddfb060749ad647ce0e60cb7c
Anwara Favour <favourchika02@gmail.com>
2023-07-07 22:21:21 +0100

    4th commit

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -8,7 +8,7 @@ const signup = async(req,res,next) => {
 
     let existingUser;
     try {
-        existingUser = await User.findOne({email: email})
+        existingUser = await User.findOne({email})
     } catch (err) {
        return console.log(err);
     }
@@ -23,15 +23,11 @@ const signup = async(req,res,next) => {
   
       const salt = await bcrypt.genSalt(10);
   
-      user.password = await bcrypt.hash(password, salt);
+      user.password = await bcrypt.hashSync(password, salt);
 
   
-      const payload = {
-        user: {
-          id: user.id,
-          name: user.name,
-        }
-      } 
+    
+       
  
 
     try {
@@ -55,7 +51,7 @@ const login = async(req,res,next) =>{
         return res.status(404).json({ message: "Couldn't find user by this password"})
     }
 
-    const isMatch = await bcrypt.compare(password, existingUser.password);
+    const isMatch = await bcrypt.compareSync(password, existingUser.password);
 
     if (!isMatch) {
       return res.status(400).json({ message: "Incorrect Password" });
@@ -65,7 +61,50 @@ const login = async(req,res,next) =>{
       });
     return res.status(200).json({message: "Login Successful", user:existingUser, token})
 
-   
+    }
+
+
+   const verifyToken = async function(req, res, next) {
+      // Get token from header
+      const authHeader = req.header('Authorization');
+      const token = authHeader.split(' ')[1];
+    
+      // Check if not token
+      if (!token) {
+        return res.status(401).json({ msg: 'No token, authorization denied' });
+      }
+    
+  try {
+    await jwt.verify(token, JWT_SECRET_KEY, (error, decoded) => {
+      if (error) {
+        res.status(401).json({ msg: 'Token is not valid' });
+      } else {
+        req.user = decoded.user;
+      }
+       console.log(user.id)
+       req.id = user.id
+    });
+  } catch (err) {
+   console.error('something wrong with auth middleware');
+    res.status(500).json({ msg: 'Server Error' });
+  }
+};
+
+
+
+const getUser = async (req, res, next) => {
+  const userId = req.id
+  let user
+  try {
+    user = await User.findById(userId, "-password")
+  } catch (err) {
+    return new Error(err)
+  }
+  if(!user){
+    return res.status(400).json({message:"User Not Found"})
+  }
+  return res.status(200).json({user})
 }
 
-module.exports = { signup, login};
+
+module.exports = { signup, login, verifyToken, getUser};
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { signup, login } = require('../controllers/user-controller')
+const { signup, login, verifyToken, getUser } = require('../controllers/user-controller')
 
 const router = express.Router()
 
 
 router.post("/signup",signup)
 router.post("/login", login)
+router.get("/user", verifyToken, getUser)
 
 module.exports = router;
